336f1433ab9211a827ae23a9545c46fc4120bc2e
ilnomR0 <jmcastrommx@gmail.com>
2025-10-12 21:54:09 -0700

    updated the homepage of paper productions to include all of the recent and to-be-worked on projects. Will work on more later

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 4330;
+
+const port = parseInt(process.argv[2]);
+
+if(isNaN(port)){
+  console.error("ERROR: port argument is not valid. Please supply one or change it to a valid number");
+  return 0;
+}
+
+
 
 app.use(express.static("frontend"));
-
-app.set("styleGuide", "frontend/assets/styleGuide");
+app.use("/styleGuide", express.static("frontend/assets/styleGuidelines/"))
 
 app.listen(port, (err)=>{
     console.log(`home page listening on port ${port}`);
